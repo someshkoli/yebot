b60fa85a23b1772ed23a2993b80ae4b6ebcf2e2d
SOMESH KOOLI <kolisomesh27@gmail.com>
2019-03-08 00:12:13 +0530

    buttons added

--- telbot/views.py
+++ telbot/views.py
@@ -5,37 +5,29 @@ from django.utils.html import escape
 import pyrebase
 import requests
 import json
-# Create your views here.
-"""config = {
-  "apiKey": "AIzaSyCMTjIY5xAjzz4rngCMQWygVcNNVgQlvFg",
-  "authDomain": "telbot-9c138.firebaseapp.com ",
-  "databaseURL": "https://telbot-9c138.firebaseio.com/",
-  "storageBucket": "telbot-9c138.appspot.com/"
-}
-firebase=pyrebase.initialize_app(config) """
-#
 from django.views.decorators.csrf import csrf_exempt
-
+# Create your views here.
 @csrf_exempt 
 
 def index(request):
 	query=json.load(request)
-	if query['message']['text'] is '/get_man':
+	data=query
+	print(data['message']['text'])
+	if data['message']['text']=="/get_man":
 		get_man(request)
-	else:
+	elif data['message']['text'] is "/get_assig":
 		get_assig(request)
+	else:
+		false_request()
 	return HttpResponse("true")
 def get_man(request):
-	"""return render(request,
-                        "https://api.telegram.org/bot727019423:AAEnYQDHDKygojIJovPpAttzskvS6bAaB5g/sendMessage?chat_id=573080922&text=tesst"
-                        )"""
-	return requests.post('https://api.telegram.org/bot727019423:AAEnYQDHDKygojIJovPpAttzskvS6bAaB5g/sendMessage',json={'chat_id':573080922,'text':str(json.load(request))+"man"})
+	
+	return requests.post('https://api.telegram.org/bot727019423:AAEnYQDHDKygojIJovPpAttzskvS6bAaB5g/sendMessage',json={'chat_id':573080922,'text':"select subject for manual",'reply_markup':{'inline_keyboard':[[{'text':'cg','url':'www.google.com'}],[{'text':'ostl','url':'www.google.com'}],[{'text':'am','url':'www.google.com'}],[{'text':'coa','url':'www.google.com'}],[{'text':'aoa','url':'www.google.com'}]]}})
 def get_assig(request):
-	"""return render(request,
-                        "https://api.telegram.org/bot727019423:AAEnYQDHDKygojIJovPpAttzskvS6bAaB5g/sendMessage?chat_id=573080922&text=data"
-                        )"""
-	return requests.post('https://api.telegram.org/bot727019423:AAEnYQDHDKygojIJovPpAttzskvS6bAaB5g/sendMessage',json={'chat_id':573080922,'text':str(json.load(request))+"assig"})
-
+	return requests.post('https://api.telegram.org/bot727019423:AAEnYQDHDKygojIJovPpAttzskvS6bAaB5g/sendMessage',json={'chat_id':573080922,'text':"select subject for assignment",'reply_markup':{'inline_keyboard':[[{'text':'cg','url':'www.google.com'}],[{'text':'ostl','url':'www.google.com'}],[{'text':'am','url':'www.google.com'}],[{'text':'coa','url':'www.google.com'}],[{'text':'aoa','url':'www.google.com'}]]}})
+def false_request():
+	return requests.post(url='https://api.telegram.org/bot727019423:AAEnYQDHDKygojIJovPpAttzskvS6bAaB5g/sendMessage',json={'chat_id':573080922,'text':"false request please enter a valid request"})
+	
 """def up(request):
 	file=open
 	chat_id=request['result']['from']['id']
